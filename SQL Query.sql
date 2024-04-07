e5ac6d9153cb4f625127df61df044a99f565b65d
Daniel De La Cruz <157164355+Daniel-De-la-Cruz-Vill@users.noreply.github.com>
2024-04-07 17:25:59 -0400

    Add files via upload

--- SQL Query.sql
+++ SQL Query.sql
@@ -1,6 +1,6 @@
-
 SELECT *
 FROM PizzaStore.dbo.pizza_sales
+
 -----------------------------------------------------------------------------------------------------------
 --KPIs:
 
@@ -33,7 +33,6 @@ FROM PizzaStore.dbo.pizza_sales
 GROUP BY DATENAME(DW, order_date)
 ORDER BY COUNT(DISTINCT(order_id)) DESC
 
-
 --Number of orders each month
 SELECT DATENAME(Month, order_date) AS order_month, COUNT(DISTINCT(order_id)) AS Total_orders
 FROM PizzaStore.dbo.pizza_sales
@@ -43,15 +42,14 @@ ORDER BY COUNT(DISTINCT(order_id)) DESC
 -----------------------------------------------------------------------------------------------------------
 --Percentage of sales by categories
 
---%Sales by type of pizza
+--%Revenue by type of pizza
 SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(8,2)) AS total_revenue, 
 CAST(SUM(total_price)*100/ (SELECT SUM(total_price) FROM PizzaStore.dbo.pizza_sales) AS DECIMAL(4,2)) AS Percentage
 FROM PizzaStore.dbo.pizza_sales
 GROUP BY pizza_category
 ORDER BY SUM(total_price) DESC
 
-
---%Sales by size of pizza
+--%Revenue by size of pizza
 SELECT pizza_category, pizza_size, CAST(SUM(total_price) AS DECIMAL(8,2)) AS Revenue_by_size, 
 CAST(SUM(total_price)*100 / (SELECT SUM(total_price) FROM PizzaStore.dbo.pizza_sales) AS DECiMAL (4,2)) AS Percentage
 FROM PizzaStore.dbo.pizza_sales
@@ -65,17 +63,52 @@ GROUP BY pizza_category
 ORDER BY SUM(total_price) DESC
 
 -----------------------------------------------------------------------------------------------------------
---Top and Bottom 5 by revenue
+--Top and Bottom 5 pizzas by quantity sold, revenue, and presence in orders
 
---Top 5 Pizzas by revenue
+--Top 5 Pizzas by quantity sold
 SELECT TOP 5 pizza_name, SUM(Quantity) AS total_units_sold
 FROM PizzaStore.dbo.pizza_sales
 GROUP BY pizza_name
 ORDER BY SUM(Quantity) DESC
- 
---Bottom 5 Pizzas by revenue
+
+--Top 5 pizzas by orders
+SELECT TOP 5 pizza_name, COUNT(DISTINCT(order_id)) number_of_orders
+FROM PizzaStore.dbo.pizza_sales
+GROUP BY pizza_name
+ORDER BY number_of_orders DESC
+
+--Top 5 pizzas by revenue
+WITH a as (
+SELECT pizza_name, pizza_size, unit_price, 
+SUM(quantity) number_of_pizzas_sold, SUM(quantity)*unit_price revenue
+FROM PizzaStore.dbo.pizza_sales
+GROUP BY pizza_name, pizza_size, unit_price)
+
+SELECT TOP 5 pizza_name, SUM(revenue) total_revenue
+FROM a
+GROUP BY pizza_name
+ORDER BY total_revenue DESC
+
+--Bottom 5 Pizzas by quantity sold
 SELECT TOP 5 pizza_name, SUM(Quantity) AS total_units_sold
 FROM PizzaStore.dbo.pizza_sales
 GROUP BY pizza_name
 ORDER BY SUM(Quantity) ASC
 
+--Bottom 5 pizzas by orders
+SELECT TOP 5 pizza_name, COUNT(DISTINCT(order_id)) number_of_orders
+FROM PizzaStore.dbo.pizza_sales
+GROUP BY pizza_name
+ORDER BY number_of_orders ASC
+
+--Bottom 5 pizzas by revenue
+WITH a as (
+SELECT pizza_name, pizza_size, unit_price, 
+SUM(quantity) number_of_pizzas_sold, SUM(quantity)*unit_price revenue
+FROM PizzaStore.dbo.pizza_sales
+GROUP BY pizza_name, pizza_size, unit_price)
+
+SELECT TOP 5 pizza_name, SUM(revenue) total_revenue
+FROM a
+GROUP BY pizza_name
+ORDER BY total_revenue ASC
